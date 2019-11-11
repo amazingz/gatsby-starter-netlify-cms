95ffb38191e2c811da1d72612518e3e16f63f229
Vadym Brodetskyi <amazingtr@gmail.com>
2019-11-11 02:25:20 +0200

    changed

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -91,18 +91,18 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
-                      See all products
+                      Послуги
                     </Link>
                   </div>
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Останні публікації
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/blog">
-                      Read more
+                      Детальніше..
                     </Link>
                   </div>
                 </div>
